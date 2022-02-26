5fd626ff3eec4b50e531a7778e4e08de95c33c51
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-26 20:20:19 +0500

    added styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {useSelector, useDispatch} from "react-redux";
 import {increment, decrement, SIGN_IN} from "./actions";
 
@@ -9,12 +10,44 @@ function App() {
     const dispatch = useDispatch();
     return (
         <div>
-            <h1>Counter: {counter}</h1>
-            <button onClick={() => dispatch(increment())}>+</button>
-            <button onClick={() => dispatch(increment(5))}>+5</button>
-            <button onClick={() => dispatch(decrement())}>-</button>
-            <button onClick={() => dispatch(SIGN_IN())}>LOGIN</button>
-            {loggedIn && (<div>Hello User!</div>)}
+            <header className={'text-center bg-primary text-white p-4'}>
+                <h1>REACT REDUX COUNTER</h1>
+            </header>
+            <div className={"container"}>
+                <div className="text-center">
+                    <div className={'border round-2 mt-5 m-auto p-5 height-50 myCard'}>
+                        <h1 className={"pb-5"}>Counter: {counter}</h1>
+                        <div className="row mb-4 mt-5">
+                            <div className="col-6 d-grid">
+                                <button onClick={() => dispatch(increment())}
+                                        className={'btn btn-primary round-1'}>+
+                                </button>
+                            </div>
+                            <div className="col-6 d-grid">
+                                <button onClick={() => dispatch(decrement())}
+                                        className={'btn btn-primary'}>-
+                                </button>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12 d-grid mb-2">
+                                <button onClick={() => dispatch(increment(5))}
+                                        className={'btn btn-outline-primary'}>+5
+                                </button>
+                            </div>
+                            <div className="col-12 d-grid">
+                                <button onClick={() => dispatch(SIGN_IN())}
+                                        className={'btn btn-outline-success'}>LOGIN
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className={'h2 mt-4'}>
+                            {loggedIn && (<div>Hello User!</div>)}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
